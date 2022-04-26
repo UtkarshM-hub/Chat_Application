637b4ef3df66adfe60a8ac19ff6a5b1ebf52c1ec
Utkarsh Mandape <70505181+UtkarshM-hub@users.noreply.github.com>
2022-04-26 10:10:24 +0530

    security changes

--- Backend/app.js
+++ Backend/app.js
@@ -20,7 +20,7 @@ var InventoryRouter = require('./routes/Inventory');
 const socket = require('../frontend/src/socket');
 
 // declerations
-const MONGODB_URI='mongodb+srv://UtMandape:1BGR3QO2fcFmFHXw@cluster0.akibk.mongodb.net/Chat?retryWrites=true&w=majority';
+const MONGODB_URI=YOUR_MONGODB_URI;
 
 
 // view engine setup
