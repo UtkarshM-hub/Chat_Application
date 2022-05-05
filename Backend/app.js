2d90c3387499e4d1a4dbc40f1ad121bdba795b81
Utkarsh Mandape <70505181+UtkarshM-hub@users.noreply.github.com>
2022-05-05 15:56:34 +0530

    SECURITY CHANGES

--- Backend/app.js
+++ Backend/app.js
@@ -22,7 +22,7 @@ var Payment = require('./routes/Payment');
 const socket = require('../frontend/src/socket');
 
 // declerations
-const MONGODB_URI='mongodb+srv://UtMandape:1BGR3QO2fcFmFHXw@cluster0.akibk.mongodb.net/Chat?retryWrites=true&w=majority';
+const MONGODB_URI='YOUR_MONGODB_URI';
 
 
 // view engine setup
